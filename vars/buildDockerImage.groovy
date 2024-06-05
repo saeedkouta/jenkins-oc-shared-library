bb65de0512d19c83cc8c50b516678ab7736e4ea9
Saeed Kouta <167209058+saeedkouta@users.noreply.github.com>
2024-06-05 23:41:25 +0300

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -3,6 +3,6 @@ def call(String imageName) {
 
         // Build and push Docker image
         echo "Building Docker image..."
-        sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
+        sh "docker build ${imageName}:${BUILD_NUMBER} ."
  
 }
