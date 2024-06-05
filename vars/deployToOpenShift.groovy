1bbe51ac6b1a5ef66c40029c5dfed87da804ef15
Saeed Kouta <167209058+saeedkouta@users.noreply.github.com>
2024-06-06 00:19:30 +0300

    Update deployToOpenShift.groovy

--- vars/deployToOpenShift.groovy
+++ vars/deployToOpenShift.groovy
@@ -2,13 +2,13 @@
 
 //OpenShiftCredentialsID can be credentials of service account token or KubeConfig file 
 def call(String OpenShiftCredentialsID, String openshiftClusterurl, String openshiftProject, String imageName) {
-     withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) { 
-            sh """
-        oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify
-        oc project ${project}
-        oc apply -f deployment.yml
-        oc apply -f service.yml
-        """
+    
 
+    // login to OpenShift Cluster via cluster url & service account token
+    withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
+            sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
+            sh "oc apply -f deployment.yml"
+            sh "oc apply -f service.yml
     }
+
 }
