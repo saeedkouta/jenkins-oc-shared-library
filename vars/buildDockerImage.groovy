3b3f8371a14d64c2c2b0764ea3d4ce72e4c65122
Saeed Kouta <167209058+saeedkouta@users.noreply.github.com>
2024-06-05 20:29:50 +0300

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,6 +1,6 @@
 def call(String registry, String imageName, String buildId) {
     docker.build("${registry}/${imageName}:${buildId}").inside {
-        sh "docker push ${registry}/${imageName}:${buildId}"
+        sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
     }
 }
 
