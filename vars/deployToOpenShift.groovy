6c430b9c632a308f2e145045ae8e3d755f4089ce
Saeed Kouta <167209058+saeedkouta@users.noreply.github.com>
2024-06-05 21:38:02 +0300

    Update deployToOpenShift.groovy

--- vars/deployToOpenShift.groovy
+++ vars/deployToOpenShift.groovy
@@ -1,10 +1,16 @@
-def call(String server, String project, String imageName, String registry, String buildId, String credentialsId) {
-    withCredentials([string(credentialsId: "${credentialsId}", variable: 'TOKEN')]) {
-        sh """
-        oc login ${server} --token=${TOKEN}
-        oc project ${project}
-        oc set image deployment/${imageName} ${imageName}=${registry}/${imageName}:${buildId} --namespace=${project}
-        """
+#!/usr/bin/env groovy
+
+//OpenShiftCredentialsID can be credentials of service account token or KubeConfig file 
+def call(String OpenShiftCredentialsID, String openshiftClusterurl, String openshiftProject, String imageName) {
+    
+    // Update deployment.yaml with new Docker Hub image
+    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"
+
+    // login to OpenShift Cluster via cluster url & service account token
+    withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
+            sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
+            sh "oc apply -f ."
     }
+
 }
 
